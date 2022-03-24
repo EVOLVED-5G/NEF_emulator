d8cae00a7ba1653790e97a2f434758d5080b998a
andimintra <angela.dimitriou@intrasoft-intl.com>
2022-03-24 12:34:32 +0200

    updated pipeline

--- pipelines/robot-testing/nef_emulator_testing.groovy
+++ pipelines/robot-testing/nef_emulator_testing.groovy
@@ -87,8 +87,8 @@ pipeline{
                             -v ${ROBOT_TESTS_DIRECTORY}:/opt/robot-tests/tests \
                             -v ${ROBOT_RESULTS_DIRECTORY}/AsSessionWithQoSAPI:/opt/robot-tests/results/AsSessionWithQoSAPI \
                             -v ${ROBOT_RESULTS_DIRECTORY}/MonitoringEventAPI:/opt/robot-tests/results/MonitoringEventAPI \
-                            --variable NGINX_HOSTNAME:${NGINX_HOSTNAME} \
                             ${ROBOT_IMAGE_NAME}:${ROBOT_VERSION} \
+                            --variable NGINX_HOSTNAME:${NGINX_HOSTNAME} \
                             /bin/bash \
                             -c "robot --outputdir /opt/robot-tests/results/AsSessionWithQoSAPI /opt/robot-tests/tests/features/NEF_AsSessionWithQoS_API/nef_subscriptions_api.robot"
                     """
