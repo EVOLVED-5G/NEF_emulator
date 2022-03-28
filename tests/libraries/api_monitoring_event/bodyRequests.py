c3e16397e5c10ce9c4047e219ef29b0672366c4b
andimintra <angela.dimitriou@intrasoft-intl.com>
2022-03-28 17:46:19 +0300

    use container name

--- tests/libraries/api_monitoring_event/bodyRequests.py
+++ tests/libraries/api_monitoring_event/bodyRequests.py
@@ -1,7 +1,7 @@
 def monitoring_event_sub_body():
     return {
         "externalId": "10003@domain.com",
-        "notificationDestination": "http://localhost:80/api/v1/utils/monitoring/callback",
+        "notificationDestination": "http://nef-emulator_backend_1:80/api/v1/utils/monitoring/callback",
         "monitoringType": "LOCATION_REPORTING",
         "maximumNumberOfReports": 2,
         "monitorExpireTime": "2022-03-25T13:00:48.634Z"
@@ -10,7 +10,7 @@ def monitoring_event_sub_body():
 def one_time_monitoring_event_body():
     return {
         "externalId": "10003@domain.com",
-        "notificationDestination": "http://localhost:80/api/v1/utils/monitoring/callback",
+        "notificationDestination": "http://nef-emulator_backend_1:80/api/v1/utils/monitoring/callback",
         "monitoringType": "LOCATION_REPORTING",
         "maximumNumberOfReports": 1,
         "monitorExpireTime": "2022-03-25T13:00:48.634Z"        
